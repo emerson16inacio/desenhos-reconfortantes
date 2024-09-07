667820649ff5342e0263e442152c8fb4968d9a71
emerson16inacio <64021161+emerson16inacio@users.noreply.github.com>
2024-09-07 01:03:47 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -3,21 +3,21 @@ let dados = [
         titulo: "Avatar a Lenda de Aang: As Histórias de Ba Sing Se",
         descricao: "As Histórias de Ba Sing Se é um episódio crucial em Avatar: A Lenda de Aang que nos leva para a vida cotidiana dos personagens em Ba Sing Se. Enquanto se preparam para a invasão, cada membro da equipe enfrenta desafios pessoais: Aang busca respostas espirituais, Katara lida com seus sentimentos, Sokka com suas inseguranças, Zuko com sua solidão e Toph com sua necessidade de conexão. O episódio humaniza os personagens, mostrando suas vulnerabilidades e fortalezas, e prepara o terreno para os próximos eventos da série.",
         link: "https://pluto.tv/br/on-demand/series/601ab1ede5b36a001a4023b1/season/2/episode/65c636ec5884b400132ffe7d",
-        tags: "nick nickelodeon elementos luto redenção auto descobrimento",
+        tags: "nick nickelodeon elementos luto redenção auto descobrimento espiritualidade, guerra, paz interior, busca por significado, amadurecimento, responsabilidades, destino, elementos, cultura oriental, filosofia",
         imagem: "imagens/avatar-a-lenda-poster.jpg"
     },
     {
         titulo: "Steven Universe: Senhor Greg" ,
         descricao: "Um episódio emocionante que explora a jornada de superação de Steven e Greg após a perda de Rose Quartz. Através de uma viagem para Empire City, pai e filho reavivam a paixão pela música de Greg e encontram consolo na criação artística. O episódio mostra como a música pode ser um poderoso instrumento de cura e como a família pode unir-se diante da adversidade, celebrando a vida e a memória de quem se foi. A canção 'Here Comes a Comet' se torna um hino para a superação e a esperança, simbolizando o renascimento emocional de ambos os personagens.",
         link: "https://stevenuniverso.fandom.com/pt-br/wiki/Senhor_Greg",
-        tags: "cartoon network gemas reconciliação perda",
+        tags: "cartoon network gemas reconciliação perda amor maternal, perda de um ente querido, magia, cristais, alienígenas, autoestima, aceitação, comunidade LGBTQIA+, música como terapia",
         imagem: "imagens/steven-universo-poster.jpg"
     },
     {
         titulo: "Hey Arnold!: Dia dos Pais",
         descricao: "um episódio emocionante que celebra o papel dos pais na vida das crianças. Arnold, apesar de não ter seus pais presentes, sente a falta deles e reflete sobre o amor e cuidado que recebeu dos avós. A história mostra como a figura paterna, seja biológica ou não, desempenha um papel fundamental na criação e no desenvolvimento de um indivíduo. Através de competições e histórias, o episódio enfatiza a importância do carinho, da proteção e do apoio que os pais proporcionam, mostrando que o amor paterno pode se manifestar de diversas formas.",
         link: "https://pluto.tv/br/on-demand/series/62f295609bd9c6001b622557/season/3/episode/62f3f93985bbed001af689a6?lang=en",
-        tags: "nick nickelodeon bigorna cabeça aceitação",
+        tags: "nick nickelodeon bigorna cabeça aceitação infância, família substituta, valores familiares, comunidade, vizinhança, nostalgia, humor, amizade, lições de vida",
         imagem: "imagens/hey-arnold-poster.jpg"
 
     },
@@ -25,29 +25,30 @@ let dados = [
         titulo: "O Incrível Mundo de Gumball" ,
         descricao: "Gumball passa por um período de tristeza e irritabilidade, e seus sentimentos de isolamento e negatividade são explorados de forma bastante realista para um desenho animado infantil. É um episódio importante por abordar um tema tão complexo de maneira acessível e leve, ajudando crianças e adolescentes a entenderem melhor essa emoção.",
         link: " https://tv.apple.com/br/episode/a-depressao/umc.cmc.1i8z4yenthfl8pfa7s6knjpo?showId=umc.cmc.2kuskm7qkyoepme6044n107ym",
-        tags: "cartoon network depreção gato azul pensamentos depreção",
+        tags: "cartoon network depreção gato azul pensamentos depreção humor absurdo, surrealismo, família disfuncional, depressão, saúde mental, adolescência, relações interpessoais, comédia negra",
         imagem: "imagens/gumball.jpg"
     },
     {
         titulo: "Bluey - Conversa Virtual",
         descricao: "Bluey e Bingo exploram o mundo digital e as dificuldades de se comunicar online, abordando temas como amizade virtual e a importância da interação interpessoal.",
         link: "[https://www.youtube.com/watch?v=x7gonAtXAN8",
-        tags: "bluey cachorro animação infantil amizade tecnologia obediencia",
+        tags: "bluey cachorro animação infantil amizade tecnologia obediencia imaginação infantil, brincadeiras, família, relações entre irmãos, educação infantil, desenvolvimento infantil, cotidiano, humor leve",
         imagem: "imagens/bluey.jpg"
     },
     {
         titulo: "Super Choque na África - 3° Temporada ep 3",
         descricao: "Super Choque enfrenta um desafio pessoal que o obriga a confrontar seus medos e encontrar sua força interior. O episódio explora temas como identidade, superação e a importância de ser autêntico.",
         link: "https://www.google.com/search?q=epis%C3%B3dio+29+de+Super+Choque+na+%C3%81frica+ptbr&sca_esv=e1c72adf5cbd18e3&sca_upv=1&sxsrf=ADLYWIKyyAMTqSzQ5h0qcPQMnWT4DH2-Ug%3A1725674185781&ei=ybLbZvOkL_vW1sQP1tD34Aw&ved=0ahUKEwizrd2m3a-IAxV7q5UCHVboHcwQ4dUDCBA&oq=epis%C3%B3dio+29+de+Super+Choque+na+%C3%81frica+ptbr&gs_lp=Egxnd3Mtd2l6LXNlcnAiLGVwaXPDs2RpbyAyOSBkZSBTdXBlciBDaG9xdWUgbmEgw4FmcmljYSBwdGJyMggQABiABBiiBEjpQ1CHCFi1IXACeAGQAQCYAbgBoAGRBKoBAzAuNLgBDMgBAPgBAZgCBqACqQTCAgcQIxiwAxgnwgIKEAAYsAMY1gQYR5gDAIgGAZAGCZIHAzIuNKAHnQo&sclient=gws-wiz-serp",
-        tags: "super choque africa super herois identidade afrofuturismo liberdade",
+        tags: "super choque africa super herois identidade afrofuturismo liberdade super heróis, identidade cultural, afrofuturismo, superação, empoderamento, racismo, preconceito, amizade, família",
         imagem: "imagens/super_choque.png"
     },
     {
         titulo: "X-Men Evolution - Andando na corda bamba",
         descricao: "Os X-Men enfrentam uma missão perigosa que testa seus limites e explora a importância da confiança e do trabalho em equipe. O episódio também aborda temas como a pressão para ser perfeito e a aceitação das diferenças.",
         link: "https://www.google.com/search?q=X-Men+Evolution+-+Andando+na+corda+bamba+ptbr&sca_esv=e1c72adf5cbd18e3&sca_upv=1&biw=1366&bih=641&sxsrf=ADLYWILl8YjDZZuo20iIkHEU3if9lqLiJA%3A1725674396414&ei=nLPbZvv9GNey1sQPr_660QY&ved=0ahUKEwj7u5WL3q-IAxVXmZUCHS-_LmoQ4dUDCBA&uact=5&oq=X-Men+Evolution+-+Andando+na+corda+bamba+ptbr&gs_lp=Egxnd3Mtd2l6LXNlcnAiLVgtTWVuIEV2b2x1dGlvbiAtIEFuZGFuZG8gbmEgY29yZGEgYmFtYmEgcHRickiRFFCaAljREXABeACQAQCYAaMBoAG0BaoBAzAuNbgBA8gBAPgBAZgCAaACBsICCxAAGIAEGLADGKIEmAMAiAYBkAYBkgcBMaAH8QQ&sclient=gws-wiz-serp",
-        tags: "xmen xman mutantes super herois amizade evolução familia",
+        tags: "xmen xman mutantes super herois amizade evolução mutações, superpoderes, preconceito, discriminação, amizade, família, equipe, trabalho em equipe, superação, aceitação",
         imagem: "imagens/xmen-evolution.jpg"
     }
 
 ];
+
